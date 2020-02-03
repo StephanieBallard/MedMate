0a3c249daf761418d97c2893e734794fd0d9077c
StephanieBallard <58450565+StephanieBallard@users.noreply.github.com>
2020-02-02 22:44:05 -0800

    Add files via upload

---
+++ MedMate/MedMate/MedicationModel.swift
@@ -0,0 +1,17 @@
+//
+//  MedicationModel.swift
+//  MedMate
+//
+//  Created by Stephanie Ballard on 2/2/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import Foundation
+
+struct Medication {
+    var name: String
+    var dose: Double
+    //var time:
+    //var isTaken: Bool
+    
+}
